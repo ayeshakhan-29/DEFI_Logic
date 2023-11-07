3b101c5e8771dc1cd366323fe20726c21a73e7cf
ayeshakhan-29 <aarkk.29@gmail.com>
2023-11-07 05:38:36 +0500

    made global.d.ts file

---
+++ global.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-transition-group' {
+    export * from 'react-transition-group';
+    export { default as Transition } from 'react-transition-group/Transition';
+    export { default as CSSTransition } from 'react-transition-group/CSSTransition';
+    export { default as SwitchTransition } from 'react-transition-group/SwitchTransition';
+}
